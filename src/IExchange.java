2acb700bb8d67bbc0b35ba6329a1c30f49fd029a
Tim Rensen <t.rensen@student.tudelft.nl>
2013-12-16 21:34:39 +0100

    Adds interface for exchanges + dummy Pair class

--- src/IExchange.java
+++ src/IExchange.java
@@ -1,4 +1,10 @@
+import java.util.List;
 
 public interface IExchange {
-
+	/**
+	 * A list containing all supported Pairs
+	 */
+	//private List<Pair> supportedPairs;
+	
+	public Pair updatePair(Pair pair);
 }
