488896b0f83feebf22554bb83662dddada6c41d9
Erwin van Eyk <Erwin_dso@hotmail.com>
2013-12-18 21:42:06 +0100

    Test cases CurrencyType
    Don't forget to enable assertion checking. (VM: -ea)

---
+++ src/test/models/CurrencyTypeTest.java
@@ -0,0 +1,73 @@
+package test.models;
+
+import static org.junit.Assert.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import models.CurrencyType;
+
+import org.junit.Test;
+
+public class CurrencyTypeTest {
+
+	@Test
+	public void getInstanceTestNotNull() throws IOException {
+		CurrencyType type = CurrencyType.getInstance("usd");
+		assertNotNull(type);
+	}
+	
+	@Test(expected = IOException.class)
+	public void getInstanceTestException() throws IOException {
+		CurrencyType.getInstance("NUL");
+	}
+	
+	@Test
+	public void constructorTest() {
+		CurrencyType type = new CurrencyType("usd","dollar","$",2);
+		assertNotNull(type);
+	}
+
+	@Test(expected = AssertionError.class)
+	public void constructorTestBadSign1() {
+		new CurrencyType("usd","dollar","$",-1);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void constructorTestBadSign2() {
+		new CurrencyType("usd","dollar","$",20);
+	}
+
+	@Test(expected = AssertionError.class)
+	public void constructorTestBadCode() {
+		new CurrencyType("usdfoo","dollar","$",2);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void constructorTestNull1() {
+		new CurrencyType(null,"dollar","$",2);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void constructorTestNull2() {
+		new CurrencyType("usd",null,"$",2);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void constructorTestNull3() {
+		new CurrencyType("usd","dollar",null,2);
+	}
+	
+	@Test
+	public void getTest() throws IOException {
+		String code = "usd";
+		Properties prop = new Properties();
+		prop.load(new FileInputStream(CurrencyType.SETTINGS_PATH));
+		CurrencyType type = CurrencyType.getInstance(code);
+		assertEquals(type.getISOCode(),code.toUpperCase());
+		assertEquals(type.getName(),prop.getProperty(code + "_name"));
+		assertEquals(type.getSignificance(),Integer.parseInt(prop.getProperty(code + "_sign")));
+		assertEquals(type.getSymbol(),prop.getProperty(code + "_symbol"));
+	}
+}
