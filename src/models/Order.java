8255c38665885b859aab77ba86bf17456cfafd04
Erwin van Eyk <Erwin_dso@hotmail.com>
2013-12-20 18:12:15 +0100

    Modified Order
    Added comments, changed pairType to pair (so now it is linked to an
    exchange)

--- src/models/Order.java
+++ src/models/Order.java
@@ -1,5 +1,10 @@
 package models;
 
+/**
+ * The Order-class contains information about a placed or executed order on a exchange
+ * @author Erwin
+ *
+ */
 public class Order {
 	/**
 	 * The exchange-bound id of the order. 
@@ -7,21 +12,42 @@ public class Order {
 	private long id;
 	
 	/**
-	 *
+	 * The pair on which the order is based.
 	 */
-	private PairType pairType;
+	private Pair pair;
 	
+	/**
+	 * Buy or sell order?
+	 */
 	private int type; //BUY / SELL
 	
+	/**
+	 * Amount of Currency to trade.
+	 */
 	private Currency amount;
 	
+	/**
+	 * The rate at which the order should be executed.
+	 */
 	private double rate;
 	
+	/**
+	 * The timestamp of the creation of the order.
+	 */
 	private long timestamp;
 
-	public Order(long id, PairType pairType, int type, Currency amount, double rate, long timestamp) {
+	/**
+	 * Constructor
+	 * @param id The id of the order, often provided by the exchange API.
+	 * @param pair The pair related to this order.
+	 * @param type Whether this is a BUY or SELL order.
+	 * @param amount The amount associated with this order.
+	 * @param rate The rate/price at which the order should be executed at.
+	 * @param timestamp The time at which the order was placed on the exchange.
+	 */
+	public Order(long id, Pair pair, int type, Currency amount, double rate, long timestamp) {
 		this.id = id;
-		this.pairType = pairType;
+		this.pair = pair;
 		this.type = type;
 		this.amount = amount;
 		this.rate = rate;
@@ -32,8 +58,8 @@ public class Order {
 		return id;
 	}
 
-	public PairType getPairType() {
-		return pairType;
+	public Pair getPair() {
+		return pair;
 	}
 
 	public int getType() {
@@ -53,6 +79,6 @@ public class Order {
 	}
 	
 	public String toString() {
-		return this.getType() + " " + this.getAmount() + " @ " + this.getRate() + " " + this.getPairType();
+		return this.getType() + " " + this.getAmount() + " @ " + this.getRate() + " " + this.getPair().getType();
 	}
 }
