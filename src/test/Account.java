d833f53dfd6041ee3bc3d46407cdd4d91f7417eb
Erwin van Eyk <Erwin_dso@hotmail.com>
2014-01-15 22:26:35 +0100

    initial commit for database-libs

---
+++ src/test/Account.java
@@ -0,0 +1,32 @@
+package test;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "accounts")
+public class Account {
+	@DatabaseField(id = true) 
+	private String name;
+	
+	@DatabaseField(canBeNull = false) 
+	private String password;
+	
+	// Every database-class needs 0-arg constructor
+	public Account() {}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ src/test/ormlitetest.java
@@ -0,0 +1,50 @@
+package test;
+// http://ormlite.com/
+import java.sql.SQLException;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+public class ormlitetest {
+	
+	public static void main(String[] args) throws SQLException {
+		// this uses h2 but you can change it to match your database 
+		String databaseUrl = "jdbc:sqlite:test.db";
+		// create a connection source to our database
+		ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl);
+		
+		// instantiate the DAO to handle Account with String id
+		Dao<Account,String> accountDao = DaoManager.createDao(connectionSource, Account.class); 
+		
+		// if you need to create the 'accounts' table make this call (dont call it twice though!)
+		//TableUtils.createTable(connectionSource, Account.class);
+		
+		// create an instance of Account
+        Account account = new Account();
+        account.setName("Jim Coakley");
+        account.setPassword("password123");
+        
+        // persist the account object to the database (dont call it twice though)
+        accountDao.createIfNotExists(account);
+        // else:
+        account.setPassword("password123");
+        accountDao.update(account);
+        
+        // retrieve the account from the database by its id field (name)
+        Account account2 = accountDao.queryForId("Jim Coakley");
+        System.out.println("Account: " + account2.getPassword());
+
+        account.setPassword("PASSWORD");
+        accountDao.update(account);
+
+        // retrieve the account from the database by its id field (name)
+        Account account3 = accountDao.queryForId("Jim Coakley");
+        System.out.println("Account: " + account3.getPassword());
+        
+        // close the connection source
+        connectionSource.close();
+	}
+}
