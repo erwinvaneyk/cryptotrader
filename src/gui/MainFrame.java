7fac2db988ba1b66cfd642872a69747f78430076
Tim Rensen <t.rensen@student.tudelft.nl>
2014-01-21 21:38:00 +0100

    Adds extra methods for adding/moving JPanels

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -4,6 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -18,6 +21,7 @@ public class MainFrame extends JFrame {
 	private static final long serialVersionUID = -8026416994513756565L;
 	
 	private JPanel leftPanel, rightPanel, bottomPanel;
+	private ArrayList<JPanel> panelsLeft = new ArrayList<JPanel>();
 
 	public MainFrame() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -53,13 +57,14 @@ public class MainFrame extends JFrame {
 	private void addPanels() {
 		Container pane = getContentPane();
 		pane.setLayout(new BorderLayout());
+		((BorderLayout)pane.getLayout()).setHgap(0);
+		((BorderLayout)pane.getLayout()).setVgap(0);
 		
-		// important! Otherwise pane.getWidth will be 0.
+		// Important: pane.getWidth() will return 0 otherwise.
 		pack();
 		
 		// left panel
 		leftPanel = new JPanel();
-		leftPanel.setBackground(Color.GRAY);
 		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
 		
 		// right panel
@@ -72,15 +77,20 @@ public class MainFrame extends JFrame {
 		bottomPanel.setBackground(Color.GREEN);
 		bottomPanel.add(new JLabel("Erwin van Eyk & Tim Rensen"));
 		
-		pane.add(leftPanel,   BorderLayout.WEST);
+		JPanel p = new JPanel();
+		p.add(leftPanel);
+		p.setBackground(Color.GRAY);
+		((FlowLayout) p.getLayout()).setVgap(0);
+		((FlowLayout) p.getLayout()).setHgap(0);
+		
+		pane.add(p,   BorderLayout.WEST);
 		pane.add(rightPanel,  BorderLayout.CENTER);
 		pane.add(bottomPanel, BorderLayout.PAGE_END);
 		
 
-		JPanel p;
 		// add more specific panels		
 		p = new JPanel();
-		p.setPreferredSize(new Dimension(0, 190));
+		p.setPreferredSize(new Dimension(0, 200));
 		p.setBackground(Color.WHITE);
 		rightPanel.add(p, BorderLayout.PAGE_END);
 		
@@ -91,23 +101,85 @@ public class MainFrame extends JFrame {
 		
 		p = new JPanel();
 		p.setBackground(Color.BLACK);
-		p.setPreferredSize(new Dimension(pane.getWidth()/2 - 10, 75));
+		p.setPreferredSize(new Dimension(pane.getWidth()/2, 75));
 		p.setMaximumSize(p.getPreferredSize());
-		leftPanel.add(p);
+		addAtTop(p);
 		
 		p = new JPanel();
 		p.setBackground(Color.BLUE);
-		p.setPreferredSize(new Dimension(pane.getWidth()/2 - 10, 100));
+		p.setPreferredSize(new Dimension(pane.getWidth()/2, 100));
 		p.setMaximumSize(p.getPreferredSize());
-		leftPanel.add(p);
+		addAtBottom(p);
 		
 		p = new JPanel();
 		p.setBackground(Color.MAGENTA);
-		p.setPreferredSize(new Dimension(pane.getWidth()/2 - 10, 125));
+		p.setPreferredSize(new Dimension(pane.getWidth()/2, 125));
 		p.setMaximumSize(p.getPreferredSize());
-		leftPanel.add(p);
+		addAtBottom(p);
+		
+		
+		p = new JPanel();
+		p.setBackground(Color.RED);
+		p.setPreferredSize(new Dimension(pane.getWidth()/2, 250));
+		p.setMaximumSize(p.getPreferredSize());
+		
+		addAtBottom(p);
+		removePanel(p);
+		
+		addAtTop(p);
+		removeIndex(0);
+		
+		swapPanels(0, 1);
+		swapPanels(panelsLeft.get(0), panelsLeft.get(2));
 	}
 	
+	private void addAtBottom(JPanel p) {
+		panelsLeft.add(p);
+		
+		updateLeftPanel();
+	}
+	
+	private void addAtTop(JPanel p) {
+		panelsLeft.add(0, p);
+		
+		updateLeftPanel();
+	}
+	
+	private void removeIndex(int index) {
+		try {
+			panelsLeft.remove(index);
+			updateLeftPanel();
+		}
+		catch(IndexOutOfBoundsException e) {
+			// TODO Logger
+		}
+	}
+	
+	private void removePanel(JPanel panel) {
+		if(panelsLeft.remove(panel))
+			updateLeftPanel();
+	}
+	
+	private void swapPanels(int i, int j) {
+		try {
+			Collections.swap(panelsLeft, i, j);
+			updateLeftPanel();
+		} catch(IndexOutOfBoundsException e) {
+			// TODO Logger
+		}
+	}
+	
+	private void swapPanels(JPanel i, JPanel j) {
+		swapPanels(panelsLeft.indexOf(i), panelsLeft.indexOf(j));
+	}
+	
+	private void updateLeftPanel() {
+		leftPanel.removeAll();
+		
+		for(JPanel p : panelsLeft)
+			leftPanel.add(p);
+	}
+
 	public static void main(String[] args) {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
