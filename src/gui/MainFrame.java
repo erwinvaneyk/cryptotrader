fb9828024693fb7054686817cd9e65dca48f4aaa
Tim Rensen <t.rensen@student.tudelft.nl>
2014-01-22 19:55:15 +0100

    JPanels can move now by dragging

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -5,6 +5,9 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -17,13 +20,17 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
+@SuppressWarnings("unused")
 public class MainFrame extends JFrame {
 	private static final long serialVersionUID = -8026416994513756565L;
 	
 	private JPanel leftPanel, rightPanel, bottomPanel;
-	private ArrayList<JPanel> panelsLeft = new ArrayList<JPanel>();
+	private ArrayList<PanelFixedSize> panelsLeft = new ArrayList<PanelFixedSize>();
+	private ArrayList<Integer> prefixsum = new ArrayList<Integer>();
+	private SwapListener swapListener = new SwapListener();
 
 	public MainFrame() {
+		super("CryptoTrader");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setMinimumSize(new Dimension(800, 600));
 		setLocationRelativeTo(null);
@@ -82,10 +89,13 @@ public class MainFrame extends JFrame {
 		p.setBackground(Color.GRAY);
 		((FlowLayout) p.getLayout()).setVgap(0);
 		((FlowLayout) p.getLayout()).setHgap(0);
+		p.addMouseMotionListener(swapListener);
+		p.addMouseListener(swapListener);
 		
-		pane.add(p,   BorderLayout.WEST);
-		pane.add(rightPanel,  BorderLayout.CENTER);
-		pane.add(bottomPanel, BorderLayout.PAGE_END);
+		
+		pane.add(p,   			BorderLayout.WEST);
+		pane.add(rightPanel,  	BorderLayout.CENTER);
+		pane.add(bottomPanel, 	BorderLayout.PAGE_END);
 		
 
 		// add more specific panels		
@@ -98,51 +108,58 @@ public class MainFrame extends JFrame {
 		p.setBackground(Color.CYAN);
 		rightPanel.add(p, BorderLayout.CENTER);
 		
+		PanelFixedSize pfh;
+		pfh = new PanelFixedSize(pane.getWidth()/2, 75);
+		pfh.setBackground(Color.BLACK);
+		addAtTop(pfh);
 		
-		p = new JPanel();
-		p.setBackground(Color.BLACK);
-		p.setPreferredSize(new Dimension(pane.getWidth()/2, 75));
-		p.setMaximumSize(p.getPreferredSize());
-		addAtTop(p);
+		pfh = new PanelFixedSize(pane.getWidth()/2, 100);
+		pfh.setBackground(Color.BLUE);
+		addAtBottom(pfh);
 		
-		p = new JPanel();
-		p.setBackground(Color.BLUE);
-		p.setPreferredSize(new Dimension(pane.getWidth()/2, 100));
-		p.setMaximumSize(p.getPreferredSize());
-		addAtBottom(p);
-		
-		p = new JPanel();
-		p.setBackground(Color.MAGENTA);
-		p.setPreferredSize(new Dimension(pane.getWidth()/2, 125));
-		p.setMaximumSize(p.getPreferredSize());
-		addAtBottom(p);
-		
-		
-		p = new JPanel();
-		p.setBackground(Color.RED);
-		p.setPreferredSize(new Dimension(pane.getWidth()/2, 250));
-		p.setMaximumSize(p.getPreferredSize());
-		
-		addAtBottom(p);
-		removePanel(p);
-		
-		addAtTop(p);
-		removeIndex(0);
-		
-		swapPanels(0, 1);
-		swapPanels(panelsLeft.get(0), panelsLeft.get(2));
+		pfh = new PanelFixedSize(pane.getWidth()/2, 125);
+		pfh.setBackground(Color.MAGENTA);
+		addAtBottom(pfh);		
 	}
 	
-	private void addAtBottom(JPanel p) {
+	private void addAtBottom(PanelFixedSize p) {
 		panelsLeft.add(p);
 		
 		updateLeftPanel();
 	}
 	
-	private void addAtTop(JPanel p) {
-		panelsLeft.add(0, p);
-		
-		updateLeftPanel();
+	private void addAtTop(PanelFixedSize p) {
+		addBefore(p, 0);
+	}
+	
+	private void addAfter(PanelFixedSize a, int index) {
+		try {
+			panelsLeft.add(index+1, a);
+			
+			updateLeftPanel();
+		}
+		catch(IndexOutOfBoundsException e) {
+			// TODO Logger
+		}
+	}
+	
+	private void addAfter(PanelFixedSize a, PanelFixedSize p) {
+		addAfter(a, panelsLeft.indexOf(p));
+	}
+	
+	private void addBefore(PanelFixedSize a, int index) {
+		try {
+			panelsLeft.add(index, a);
+			
+			updateLeftPanel();
+		}
+		catch(IndexOutOfBoundsException e) {
+			// TODO Logger
+		}
+	}
+	
+	private void addBefore(PanelFixedSize a, PanelFixedSize p) {
+		addBefore(a, panelsLeft.indexOf(p));
 	}
 	
 	private void removeIndex(int index) {
@@ -176,8 +193,21 @@ public class MainFrame extends JFrame {
 	private void updateLeftPanel() {
 		leftPanel.removeAll();
 		
-		for(JPanel p : panelsLeft)
+		prefixsum.clear();
+		int sum = 0;
+		
+		for(PanelFixedSize p : panelsLeft) {
 			leftPanel.add(p);
+			sum += p.getHeight();
+			prefixsum.add(sum);
+		}
+		
+		revalidate();
+		repaint();
+		
+		/*System.out.println(panelsLeft);
+		System.out.println(prefixsum);
+		System.out.println("=====");*/
 	}
 
 	public static void main(String[] args) {
@@ -187,4 +217,86 @@ public class MainFrame extends JFrame {
 		
 		new MainFrame();
 	}
+	
+	private class SwapListener implements MouseListener, MouseMotionListener {
+		private int selected, grabY;
+		
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			if(selected != -1
+					&& e.getY() >= 0
+					&& e.getY() <= prefixsum.get(prefixsum.size()-1)) {
+				
+				// panels that can be moved down
+				if(selected < panelsLeft.size() - 1) {
+					int y1 = 0;
+					if(selected > 0)
+						y1  = prefixsum.get(selected - 1);
+					
+					int y2  = prefixsum.get(selected);
+					int y3  = prefixsum.get(selected + 1);
+					
+					int y2a = y3 - (y2 - y1) + grabY;
+
+					//System.out.println(y2a + " && " + e.getY());
+					if(e.getY() > y2a) {
+						swapPanels(selected, selected + 1);
+						selected = selected + 1;
+					}
+				}
+				
+				// panels that can be moved up
+				if(selected > 0) {
+					int y1 = 0;
+					if(selected - 1 > 0)
+						y1 = prefixsum.get(selected - 2);
+					
+					int y2  = prefixsum.get(selected - 1);
+					int y3  = prefixsum.get(selected);
+					
+					int y2a = y1 + grabY;
+
+					if(e.getY() < y2a) {
+						swapPanels(selected, selected - 1);
+						selected = selected - 1;
+					}
+				}
+			}
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			if(e.getY() > prefixsum.get(prefixsum.size()-1)) {
+				selected = -1;
+				grabY = 0;
+				return;
+			}
+			
+			int index = 0;
+			while(e.getY() > prefixsum.get(index))
+				index++;
+			
+			selected = index;
+			int start = (index == 0) ? 0 : prefixsum.get(selected - 1);
+			grabY = e.getY() - start;
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {}
+		
+		@Override
+		public void mouseClicked(MouseEvent e) {}
+		
+		@Override
+		public void mouseMoved(MouseEvent e) {}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {}
+
+		@Override
+		public void mouseExited(MouseEvent e) {}
+		
+	}
 }
+
+
